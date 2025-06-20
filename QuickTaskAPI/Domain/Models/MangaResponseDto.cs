7071487e08933a1e210020ec71eb91a9fb1f2169
AngelRey1 <angelreychidzib5@gmail.com>
2025-06-20 11:42:37 -0600

    feat: agrega CORS

--- QuickTaskAPI/Domain/Models/MangaResponseDto.cs
+++ QuickTaskAPI/Domain/Models/MangaResponseDto.cs
@@ -1,4 +1,5 @@
 namespace QuickTaskAPI.Domain.Models;
+using System.Text.Json.Serialization;
 
 public class MangaResponseDto
 {
@@ -9,12 +10,14 @@ public class MangaResponseDto
     public int Volumes { get; set; }
     public bool IsOngoing { get; set; }
     public int? GenreId { get; set; }
+    [JsonPropertyName("genero")]
     public GenreResponseDto? Genre { get; set; }
 }
 
 public class GenreResponseDto
 {
     public int Id { get; set; }
+    [JsonPropertyName("genero")]
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
 } 
